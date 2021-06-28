14de127211a06ec71fa388d1702102091a5f91c9
Raf _ <btcossin@gmail.com>
2021-06-28 17:03:04 -0400

    commiting new greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
+    res.send({ hi: 'there my good friend'});
 });
 
 const PORT = process.env.PORT || 5000;
