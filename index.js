ee8efc8e31c017a46c72c46accb4ebeb0d8f9591
Raf _ <btcossin@gmail.com>
2021-06-28 19:51:22 -0400

    commiting new greeting

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const app = express();
 
 app.get('/', (req, res) => {
     //res.send({ hi: 'there my good friend'});
-    const [book, setBook] = useState("");
-const [result, setResult] = useState([]);
-const [apiKey, setApiKey] = useState("AIzaSyBm-Omi3o6U4tNvT445DyT-eH4suqeDYTs");
     axios.get("https://www.googleapis.com/books/v1/volumes?q=The Stand&key=AIzaSyBm-Omi3o6U4tNvT445DyT-eH4suqeDYTs&maxResults=40")
     .then(data => {
       console.log(data.data.items);
       setResult(data.data.items);
     });
-    res.send("Hello"+ data);
+    res.send("Hello");
 });
 
 const PORT = process.env.PORT || 5000;
