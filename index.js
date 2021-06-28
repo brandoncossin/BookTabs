460d8088bd8e89a1ac1a59284d4e7006b80ff84e
Raf _ <btcossin@gmail.com>
2021-06-28 19:48:07 -0400

    commiting new greeting

--- index.js
+++ index.js
@@ -2,18 +2,6 @@ const express = require('express');
 import axios from 'axios';
 const app = express();
 
-function getBook(){
-const [book, setBook] = useState("");
-const [result, setResult] = useState([]);
-const [apiKey, setApiKey] = useState("AIzaSyBm-Omi3o6U4tNvT445DyT-eH4suqeDYTs");
-
-axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + apiKey + "&maxResults=40")
-    .then(data => {
-      //console.log(data.data.items);
-      setResult(data.data.items);
-      res.send({ data});
-    });
-}
 app.get('/', (req, res) => {
     //res.send({ hi: 'there my good friend'});
     const [book, setBook] = useState("");
@@ -24,7 +12,7 @@ const [apiKey, setApiKey] = useState("AIzaSyBm-Omi3o6U4tNvT445DyT-eH4suqeDYTs");
       console.log(data.data.items);
       setResult(data.data.items);
     });
-    //res.send({ data});
+    res.send("Hello"+ data);
 });
 
 const PORT = process.env.PORT || 5000;
