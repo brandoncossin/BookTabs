f1128012913ddcd1dbc54d2b10b3628044adf762
Raf _ <btcossin@gmail.com>
2021-06-28 19:38:13 -0400

    test api

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express');
+import axios from 'axios';
 const app = express();
 
+function getBook(){
+const [book, setBook] = useState("");
+const [result, setResult] = useState([]);
+const [apiKey, setApiKey] = useState("AIzaSyBm-Omi3o6U4tNvT445DyT-eH4suqeDYTs");
+
+axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + apiKey + "&maxResults=40")
+    .then(data => {
+      //console.log(data.data.items);
+      setResult(data.data.items);
+      res.send({ data});
+    });
+}
 app.get('/', (req, res) => {
-    res.send({ hi: 'there my good friend'});
+    //res.send({ hi: 'there my good friend'});
+    const [book, setBook] = useState("");
+const [result, setResult] = useState([]);
+const [apiKey, setApiKey] = useState("AIzaSyBm-Omi3o6U4tNvT445DyT-eH4suqeDYTs");
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=The Stand&key=" + apiKey + "&maxResults=40")
+    .then(data => {
+      //console.log(data.data.items);
+      setResult(data.data.items);
+      res.send({ data});
+    });
 });
 
 const PORT = process.env.PORT || 5000;
