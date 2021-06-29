c375d09a206a9a81b04ad6a55ef245c39f86defb
Raf _ <btcossin@gmail.com>
2021-06-29 11:16:11 -0400

    can run with request

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require('express');
-const axios = require('axios')
+const axios = require('axios');
 const app = express();
+var cors = require('cors');
+const apiKey = "AIzaSyBm-Omi3o6U4tNvT445DyT-eH4suqeDYTs";
 
-app.get('/', (req, res) => {
-    //res.send({ hi: 'there my good friend'});
-    /*const [result, setResult] = useState([]);
-    axios.get("https://www.googleapis.com/books/v1/volumes?q=The Stand&key=AIzaSyBm-Omi3o6U4tNvT445DyT-eH4suqeDYTs&maxResults=40")
-    .then(data => {
-      console.log(data.data.items);
-      setResult(data.data.items);
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.get('/', function(req, res){
+    let param = req.query.book
+    axios.get('https://www.googleapis.com/books/v1/volumes?q='+ param+'&key='+ apiKey+ '&maxResults=40')
+    .then(function (response) {
+        response = response.data;
+        console.log(response);
+        //console.log(response[0].volumeInfo);
+        res.send(response);
     });
-    */
-   console.log(axios.get("https://www.googleapis.com/books/v1/volumes?q=The Stand&key=AIzaSyBm-Omi3o6U4tNvT445DyT-eH4suqeDYTs&maxResults=40"))
-    res.send("hello");
 });
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT);
