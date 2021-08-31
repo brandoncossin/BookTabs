071fb1b64bcc706412b0f6669bd86b8e013546d1
Raf _ <btcossin@gmail.com>
2021-08-31 19:53:52 -0400

    route fix ftw

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ app.use(cors(corsOptions));
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
   app.use(express.static(path.resolve(__dirname, "./client/build")));
-  
+  app.get("*", function(request, response){
+    response.sendFile(path.join(__dirname, "./client/build/index.html"))
+  })
 }
 //Allows mongodb connection
 mongoose.connect(keys.mongoURI, {
