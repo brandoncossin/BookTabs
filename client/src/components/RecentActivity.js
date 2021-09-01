4ee129a39a8eb0aca205f9536c1701eaf3353276
Raf _ <btcossin@gmail.com>
2021-09-01 14:20:29 -0400

    mobile fix maybe

--- client/src/components/RecentActivity.js
+++ client/src/components/RecentActivity.js
@@ -19,14 +19,7 @@ const [isLoading, setIsLoading] = useState(true);
             });
     }
     })
-    if(isLoading){
-        return <div className="text-center">
-          <div className="spinner-border" role="status">
-            <span className="sr-only">Loading...</span>
-          </div>
-        </div>
-      }
-      else{
+    
     return (
         <div className="container-fluid">
             <h1>Activity</h1>
@@ -45,6 +38,6 @@ const [isLoading, setIsLoading] = useState(true);
             ))}
         </div>
     );
-                }
+                
 }
 export default RecentActivity;
