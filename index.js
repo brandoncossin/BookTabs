6256052eb932a4a6d1e24754891a6a8804015d3e
Raf _ <btcossin@gmail.com>
2021-08-31 21:14:13 -0400

    commmmit

--- index.js
+++ index.js
@@ -20,7 +20,15 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
+//Gets user information for account
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
 
+  const path = require('path');
+  app.get("*", function(request, response){
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ))
+  })
+}
 //Allows mongodb connection
 mongoose.connect(keys.mongoURI, {
   useNewUrlParser: true,
@@ -308,14 +316,6 @@ app.get('/profile', async (req, res) => {
   const user = await User.findOne({ uid }).lean()
   res.send({ status: 'success', profile: user });
 })
-//Gets user information for account
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
 
-  const path = require('path');
-  app.get("*", function(request, response){
-    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ))
-  })
-}
 const PORT = process.env.PORT || 8080;
 app.listen(PORT);
